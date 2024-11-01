db5cb3e72dc7ad8a2b225687e1cada5edd713475
lucas <lucas.vaugrente@etu.univ-nantes.fr>
2024-11-01 11:43:35 +0100

    add country + country code on ttl file

--- src/convert_csv_to_rdf.py
+++ src/convert_csv_to_rdf.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from rdflib import Graph, Literal, Namespace, XSD
+from rdflib import Graph, Literal, Namespace, XSD, URIRef
 import reverse_geocode
 import math
 
@@ -14,6 +14,7 @@ def csv_to_rdf_normalized_year(csv_file, rdf_file, max_rows=70000):
     DBO = Namespace("https://dbpedia.org/ontology/")
     SCHEMA = Namespace("https://schema.org/")
     EX = Namespace("https://example.org/meteorite/")
+    DBPEDIA_RESOURCE = Namespace("http://dbpedia.org/resource/")
     g.bind("ex", EX)
 
     count = 0
@@ -22,10 +23,11 @@ def csv_to_rdf_normalized_year(csv_file, rdf_file, max_rows=70000):
 
         mass = float(row['mass (g)']) if not pd.isna(row['mass (g)']) else None
         year = int(row['year']) if not pd.isna(row['year']) else None
-        country = str(row['country']) if not row['country'] == "" else None
+        country_code = str(row['country_code']) if not pd.isna(row['country_code']) else None
+        country = str(row['country']) if not pd.isna(row['country']) else None
 
-        if year and mass and country:
-            if(mass>10.0):
+        if year and mass and country_code and country:
+            if mass > 10.0:
                 g.add((meteorite_uri, SCHEMA.name, Literal(row['name'], datatype=XSD.string)))
                 g.add((meteorite_uri, DBO.recclass, Literal(row['recclass'], datatype=XSD.string)))
                 g.add((meteorite_uri, SCHEMA.weight, Literal(mass, datatype=XSD.float)))
@@ -33,31 +35,31 @@ def csv_to_rdf_normalized_year(csv_file, rdf_file, max_rows=70000):
                 g.add((meteorite_uri, SCHEMA.Event, Literal(row['fall'], datatype=XSD.string)))
                 g.add((meteorite_uri, GEONAMES.latitude, Literal(row['reclat'], datatype=XSD.float)))
                 g.add((meteorite_uri, GEONAMES.longitude, Literal(row['reclong'], datatype=XSD.float)))
-                g.add((meteorite_uri, SCHEMA.country, Literal(row['country'], datatype=XSD.string)))
+                g.add((meteorite_uri, DBO.country_code, URIRef(DBPEDIA_RESOURCE + country_code.replace(" ", "_"))))
+                g.add((meteorite_uri, DBO.country, URIRef(DBPEDIA_RESOURCE + country.replace(" ", "_"))))
             else:
                 count += 1
         else:
-            count+=1
+            count += 1
 
     g.serialize(destination=rdf_file, format="turtle")
-
     print(count)
 
-def get_country_code(lat, lon):
+def get_country_info(lat, lon):
     if not math.isnan(lon) and not math.isnan(lat):
         location = reverse_geocode.search([(lat, lon)])
-        return location[0]['country_code'] if location else None
-    return None
+        return location[0]['country_code'], location[0]['country']
+    return None, None
 
 def get_countries_meteorite():
     file_path = '../data/Meteorite_Landings_20240924.csv'
     meteorite_data = pd.read_csv(file_path)
 
-    meteorite_data['country'] = meteorite_data.apply(
-        lambda row: get_country_code(row['reclat'], row['reclong']), axis=1
+    meteorite_data[['country_code', 'country']] = meteorite_data.apply(
+        lambda row: pd.Series(get_country_info(row['reclat'], row['reclong'])), axis=1
     )
 
-    output_path = '../data/Meteorite_Landings_with_country.csv'  # Remplace avec le chemin de sortie
+    output_path = '../data/Meteorite_Landings_with_country.csv'
     meteorite_data.to_csv(output_path, index=False)
 
 # get_countries_meteorite()
